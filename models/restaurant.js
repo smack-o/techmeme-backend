21a6e287e3e6c6666961576451af5c4abc276b58
smackgg <smackgg@gmail.com>
2017-07-17 08:39:01 +0800

    更改餐厅的数据结构模式

--- models/restaurant.js
+++ models/restaurant.js
@@ -5,28 +5,35 @@ const Schema = mongoose.Schema;
 const RestaurantSchema = new Schema({
   name: String,
   price: Number,
-  picture: String,
+  picture: [String],
   topic: String,
   reason: String,
   comments: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Comment',
+    openId: String,
+    userName: String,
+    avatar: String,
+    contents: String,
+    likes: [String],
+    nominate: [String],
   }],
   address: String,
   longitude: Number,
   latitude: Number,
   businessHours: [String],
+}, {
+  timestamps: true,
 });
 
-const commentSchema = new Schema({
-  openId: String,
-  userName: String,
-  avatar: String,
-  contents: String,
-  likes: [String],
-  nominate: [String],
-});
-const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
-const Comments = mongoose.model('Comment', commentSchema);
-exports.Restaurant = Restaurant;
-exports.Comment = Comments;
+// const commentSchema = new Schema({
+//   openId: String,
+//   userName: String,
+//   avatar: String,
+//   contents: String,
+//   likes: [String],
+//   nominate: [String],
+// });
+// const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+// const Comments = mongoose.model('Comment', commentSchema);
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
+// exports.Restaurant = Restaurant;
+// exports.Comment = Comments;
