bfb93df4748b575745d275a2325f938b050de228
smackgg <smackgg@gmail.com>
2017-07-18 09:18:53 +0800

    丰富餐厅信息api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const rest = require('../comms/restaurantdb');
+const rest = require('../control/restaurantdb');
+// const problemfeed = require('../control/problemfeedbackdb');
+// const userInfo = require('../control/userInfodb');
 
 const router = express.Router();
 
+// 处理响应信息
+function handleRes(result, res) {
+  if (result.error) {
+    res.status(400).json({
+      error: result.error,
+    });
+    return;
+  }
+  res.json(result);
+}
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: '探索' });
@@ -13,6 +25,22 @@ router.get('/v1', (req, res) => {
 router.get('/v1/home', (req, res) => {
   res.render('index', { title: 'v1/home' });
 });
-router.post('/v1/addRestaurant', rest.addRestaurant);
-router.get('/v1/getRestaurant/:id', rest.getRestaurant);
+// 发布餐厅信息
+router.post('/v1/addRestaurant', (req, res) => {
+  rest.addRestaurant(req, res).then((result) => {
+    handleRes(result, res);
+  });
+});
+// 获取餐厅信息
+router.get('/v1/getRestaurant/:id', (req, res) => {
+  rest.getRestaurant(req).then((result) => {
+    handleRes(result, res);
+  });
+});
+// 更新餐厅信息
+router.put('/v1/updataRestaurant/:id', (req, res) => {
+  rest.updateRestaurant(req).then((result) => {
+    handleRes(result, res);
+  });
+});
 module.exports = router;
