922382a5d7a0164ab7b2534ec800e957dd0db609
smackgg <smackgg@gmail.com>
2018-02-06 10:35:24 +0800

    getRestaurantList services

--- admin-client/src/services/restaurant.js
+++ admin-client/src/services/restaurant.js
@@ -1,7 +1,13 @@
 import request from '../utils/request'
 
-export const addRestaurant = (data) => request({
+export const addRestaurant = data => request({
   url: '/restaurant',
   method: 'post',
+  data
+})
+
+export const getRestaurantList = data => request({
+  url: '/restaurant/list',
+  method: 'get',
   data
 })
