e95ed17c574e74e0a23572ca2b9a483d0b5ad713
smackgg <smackgg@gmail.com>
2018-02-06 14:18:20 +0800

    use moment

--- admin-client/src/App.vue
+++ admin-client/src/App.vue
@@ -17,11 +17,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 body {
-  padding: 20px;
-  margin: 0;
+  padding: 40px 20px;
 }
 </style>
