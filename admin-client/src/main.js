47b81ac809c422617d61ab7ac758f2dad11c1bf8
smackgg <smackgg@gmail.com>
2018-02-07 14:17:42 +0800

    import global less

--- admin-client/src/main.js
+++ admin-client/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
+import '@/assets/global.less'
 
 Vue.use(Element)
 
