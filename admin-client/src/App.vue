37b284bd6cfee11ccbea3a08614b3e45774e2001
smackgg <smackgg@gmail.com>
2018-02-07 14:16:38 +0800

    less move to assets

--- admin-client/src/App.vue
+++ admin-client/src/App.vue
@@ -10,16 +10,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-body {
-  padding: 40px 20px;
-}
-</style>
