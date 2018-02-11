51001f5d204a1e63426eb921d568565765a706f7
smackgg <smackgg@gmail.com>
2018-02-11 15:38:32 +0800

    nav menue

--- admin-client/src/App.vue
+++ admin-client/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
+    <nav-menu></nav-menu>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavMenu from '@/components/NavMenu'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    NavMenu
+  }
 }
 </script>
