f4fd80efbce96411fc8e51aef6461c952a78c271
smackgg <smackgg@gmail.com>
2018-08-13 16:03:49 +0800

    百度小程序测试

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'admin-client/dist')));
 
 app.all('*', (req, res, next) => {
